0341780fee76c4fa8a4f11a716e7e80854cf0f34
Salted <115632872+SaltedBone@users.noreply.github.com>
2023-06-19 21:38:32 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+body {
+    background-color: #2f3241; /* Electron background color */
+    color: #ffffff; /* Electron text color */
+    font-family: Arial, sans-serif;
+}
+
+header {
+    background-color: #d35400; /* Burnt sienna */
+    padding: 20px;
+    text-align: center;
+}
+
+footer {
+    background-color: #2f3241;
+    color: #ffffff;
+    text-align: center;
+    padding: 10px;
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+}
